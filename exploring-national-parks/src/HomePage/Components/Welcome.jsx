84379c6f7b3794dada8621f7f527f4dbcdf87c22
s0dtheg0d <82chaudrys@google.com>
2025-10-24 00:25:17 -0400

    twitter feed done

--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 /**
  * Component representing the welcome section of the homepage.
@@ -7,6 +8,12 @@ import React from 'react'
  * @returns {JSX.Element} The rendered welcome section.
  */
 const Welcome = () => {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://platform.twitter.com/widgets.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
     return (
         <div className = "welcome">
             <h1 className = "welcome-title">Explore National Parks</h1>
@@ -15,7 +22,7 @@ const Welcome = () => {
                 Click on the "Park Search" button to search for a park by activity, or click on the "Plan A Trip" button to plan a trip to a park you've already selected.
             </p>
             <div>
-
+                <a className="twitter-timeline" data-lang="en" data-height="300"  href="https://twitter.com/TempleUniv?ref_src=twsrc%5Etfw">Tweets by TempleUniv</a> 
             </div>
         </div>
     )
